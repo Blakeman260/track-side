4bf21b00554026b5dda7eb7e930e9f7a90e2955e
Blakeman260 <Andy_blakeman@hotmail.co.uk>
2021-03-09 00:15:27 +0000

    updated shopping bag to show vehicles and quantity working on product detail

--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    vehicle = None
+    if 'product_vehicle' in request.POST:
+        vehicle = request.POST['product_vehicle']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if vehicle:
+        if item_id in list(bag.keys()):
+            if vehicle in bag[item_id]['items_by_vehicle'].keys():
+                bag[item_id]['items_by_vehicle'][vehicle] += quantity
+            else:
+                bag[item_id]['items_by_vehicle'][vehicle] = quantity
+        else:
+            bag[item_id] = {'items_by_vehicle': {vehicle: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
